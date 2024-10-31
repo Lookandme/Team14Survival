f059e7946c3a3f03d770db1f4a7302e15726858d
leehokyun <urea42@naver.com>
2024-10-31 21:02:03 +0900

    [add] MainScene 상에서 합치기
    MainScene에 플레이어 추가.
    UIInventory창 Tab키 입력시 창 SetActive.

--- Assets/Scripts/UI/ItemSlot.cs
+++ Assets/Scripts/UI/ItemSlot.cs
@@ -28,8 +28,8 @@ public class ItemSlot : MonoBehaviour
 
     public void Set()
     {
-        icon.gameObject.SetActive(true);
-        icon.sprite = item.icon;
+        //icon.gameObject.SetActive(true);
+        //icon.sprite = item.icon;
         quatityText.text = quantity > 1 ? quantity.ToString() : string.Empty;
 
         if (outline != null)
@@ -41,8 +41,8 @@ public class ItemSlot : MonoBehaviour
     public void Clear()
     {
         item = null;
-        icon.gameObject.SetActive(false);
-        quatityText.text = string.Empty;
+        //icon.gameObject.SetActive(false);
+        //quatityText.text = string.Empty;
     }
 
     public void OnClickButton()

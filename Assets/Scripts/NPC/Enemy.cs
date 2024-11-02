fb8dc071ae8edc1f23efa289cc5adaff2c1e1d5d
Lookandme <kgg9386@gmail.com>
2024-11-02 16:13:03 +0900

    [Update] 애너미 로직 설정 변경

--- Assets/Scripts/NPC/Enemy.cs
+++ Assets/Scripts/NPC/Enemy.cs
@@ -40,6 +40,9 @@ public class Enemy : MonoBehaviour
 
     public float fieldOfView = 120f;
 
+    private float previousPositionX;
+
+
     private NavMeshAgent agent;
     private Animator animator;
     private SkinnedMeshRenderer[] skinnedMeshes;
@@ -52,11 +55,16 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         SetState(AIState.Wandering);
+        previousPositionX = transform.position.x; // 초기 X 위치 저장
     }
     private void Update()
     {
         playerDistance = Vector3.Distance(transform.position, CharacterManager.Instance.transform.position);
         animator.SetBool("Moving", aiState != AIState.Idle);
+        UpdateTurningAnimation();
+
+
+
 
         switch (aiState)
         {
@@ -103,6 +111,33 @@ public class Enemy : MonoBehaviour
         animator.speed = agent.speed / walkSpeed;
     }
 
+    private void UpdateTurningAnimation()    // 방향 전환 시점을 좀 더 명확하게 할 필요가 있음 개선 사항
+    {
+        float currentPositionX = transform.position.x;
+
+        if (currentPositionX > previousPositionX)
+        {
+            // 오른쪽으로 이동 중
+            animator.SetBool("TurningRight", true);
+           
+        }
+        else if (currentPositionX < previousPositionX)
+        {
+            // 왼쪽으로 이동 중
+            animator.SetBool("TurningLeft", true);
+           
+        }
+        else
+        {
+            // 움직이지 않을 때는 방향 모두 false로 설정
+            animator.SetBool("TurningRight", false);
+            animator.SetBool("TurningLeft", false);
+        }
+
+        // 이전 위치 업데이트
+        previousPositionX = currentPositionX;
+    }
+
     private void PassiveUpdate()
     {
         if (aiState == AIState.Wandering && agent.remainingDistance < 0.1f)
@@ -136,12 +171,20 @@ public class Enemy : MonoBehaviour
         if (aiState != AIState.Idle) return;
 
         SetState(AIState.Wandering);
-        agent.SetDestination(GetWanderLocation());
+        if (playerDistance > maxWanderDistance)
+        {
+            agent.SetDestination(GetWanderLocation());
+        }
+        else
+        {
+            agent.SetDestination(CharacterManager.Instance.transform.position);
+        }
+      
     }
     Vector3 GetWanderLocation()  // 평소 움직임을 갈수 있는 범위 내에서 랜덤으로 지정해주는 코드
     {
         NavMeshHit hit;
-        NavMesh.SamplePosition(transform.position + (Random.onUnitSphere * Random.Range(minWanderDistance, maxWanderDistance)), out hit, maxWanderDistance, NavMesh.AllAreas);
+        NavMesh.SamplePosition(transform.position + (Random.onUnitSphere * Random.Range(minWanderDistance, maxWanderDistance)), out hit, maxWanderDistance,NavMesh.AllAreas);
 
         int i = 0;
         while (Vector3.Distance(transform.position, hit.position) < detectDistace)  // do while 문으로 바꿔보자
